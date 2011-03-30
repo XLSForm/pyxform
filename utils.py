bbc813c4e8244bf6b7894879da60ba2f2800dc74
Andrew Marder <andrew.ei.marder@gmail.com>
2011-03-29 22:22:59 -0400

    When running this code on the server, trying to open the path described by a json string threw a UnicodeEncodeError. I've loosened what exceptions we catch, I believe this should fix the problem.

--- utils.py
+++ utils.py
@@ -42,7 +42,7 @@ def get_pyobj_from_json(str_or_path):
         # see if treating str_or_path as a path works
         fp = codecs.open(str_or_path, mode="r", encoding="utf-8")
         doc = json.load(fp, encoding="utf-8")
-    except IOError:
+    except:
         # if it doesn't work load the text
         doc = json.loads(str_or_path)
     return doc
