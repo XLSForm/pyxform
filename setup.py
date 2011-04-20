7bea23b551931924e77284380941aed2c60f3bf1
Andrew Marder <andrew.ei.marder@gmail.com>
2011-04-20 17:31:45 -0400

    Got setup.py file working well enough to pip install pyxform.

---
+++ setup.py
@@ -0,0 +1,26 @@
+import os
+from setuptools import setup
+
+# Utility function to read the README file.
+# Used for the long_description.  It's nice, because now 1) we have a top level
+# README file and 2) it's easier to type in the README file than to put a raw
+# string in below ...
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name = "pyxform",
+    version = "0.5",
+    author = "Columbia University, Modi Research Group",
+    author_email = "andrew.ei.marder@gmail.com",
+    description = ("A library for authoring XForms for ODK Collect."),
+    license = "BSD",
+    keywords = "XForm ODK Collect",
+    url = "http://github.com/mvpdev/pyxform",
+    packages=['pyxform', 'tests'],
+    long_description=read('README'),
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "License :: OSI Approved :: BSD License",
+    ],
+)
