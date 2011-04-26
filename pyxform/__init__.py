beee484b67472ea4df55d37e93fa4a006d8e6ebf
Andrew Marder <andrew.ei.marder@gmail.com>
2011-04-26 12:35:45 -0400

    Added paths field to builder so that we can handle included files gracefully.

--- pyxform/__init__.py
+++ pyxform/__init__.py
@@ -6,5 +6,6 @@ Collect easy.
 from survey import Survey
 from question import MultipleChoiceQuestion, InputQuestion
 from instance import SurveyInstance
-from builder import SurveyElementBuilder, create_survey_from_xls
+from builder import SurveyElementBuilder, create_survey_from_xls, \
+     create_survey_from_xls_or_json
 from question_type_dictionary import QuestionTypeDictionary
