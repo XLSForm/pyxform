dcf1c1ffaa042d612fad0a2be0459bf7fb83340a
Andrew Marder <andrew.ei.marder@gmail.com>
2011-04-27 17:13:06 -0400

    Added create_survey and ExcelSurveyReader to init imports.

--- pyxform/__init__.py
+++ pyxform/__init__.py
@@ -7,5 +7,6 @@ from survey import Survey
 from question import MultipleChoiceQuestion, InputQuestion
 from instance import SurveyInstance
 from builder import SurveyElementBuilder, create_survey_from_xls, \
-    create_survey_element_from_dict, render_survey_package
+    create_survey_element_from_dict, create_survey, render_survey_package
 from question_type_dictionary import QuestionTypeDictionary
+from xls2json import SurveyReader as ExcelSurveyReader
