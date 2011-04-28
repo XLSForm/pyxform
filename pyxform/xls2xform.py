d00241650c883cd5407701b5a99d9d9365303744
Andrew Marder <andrew.ei.marder@gmail.com>
2011-04-28 00:20:52 -0400

    Started using create_survey_from_path for command line tool xls2xform.

--- pyxform/xls2xform.py
+++ pyxform/xls2xform.py
@@ -8,29 +8,10 @@ To learn what sort of features are supported by xls2xform please refer
 to xls/tutorial-v0.1.xls. This example survey is the most straight
 forward introduction to the xls2xform syntax.
 """
-
 import sys
-from xls2json import SurveyReader
-from builder import create_survey_element_from_json
+from builder import create_survey_from_path
 
-def xls2json(name):
-    converter = SurveyReader("%s.xls" % name)
-    converter.print_json_to_file()
-
-def json2xform(name):
-    s = create_survey_element_from_json("%s.json" % name)
-    path = "%(name)s_%(date)s.xml" % {
-        "name" : name,
-        "date" : s.date_stamp()
-        }
-    s.print_xform_to_file(path=path)
-
-def xls2xform(path):
-    assert path.endswith(".xls")
-    name = path[:-4]
-    xls2json(name)
-    json2xform(name)
-
-if __name__=="__main__":
-    name = sys.argv[1]
-    xls2xform(name)
+if __name__ == '__main__':
+    survey = create_survey_from_path(sys.argv[1])
+    path = "%s.xml" % survey.id_string()
+    survey.print_xform_to_file(path=path)
