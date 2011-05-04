1d0ab1fc63050d9aacba5f71d681535adc6d2aa8
Andrew Marder <andrew.ei.marder@gmail.com>
2011-05-04 11:42:01 -0400

    Added some convenience imports.

--- pyxform/__init__.py
+++ pyxform/__init__.py
@@ -4,7 +4,8 @@ Collect easy.
 """
 
 from survey import Survey
-from question import MultipleChoiceQuestion, InputQuestion
+from section import Section
+from question import MultipleChoiceQuestion, InputQuestion, Question
 from instance import SurveyInstance
 from builder import SurveyElementBuilder, create_survey_from_xls, \
     create_survey_element_from_dict, create_survey, render_survey_package
