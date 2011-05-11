4cf01bab8a5075d4ed86c930e0cf6229a1f9e38f
Andrew Marder <andrew.ei.marder@gmail.com>
2011-05-11 13:58:22 -0700

    Bumped up version number.

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def read(fname):
 
 setup(
     name = "pyxform",
-    version = "0.6",
+    version = "0.7",
     author = "Columbia University, Modi Research Group",
     author_email = "andrew.ei.marder@gmail.com",
     description = ("A library for authoring XForms for ODK Collect."),
