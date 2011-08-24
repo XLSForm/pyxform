dab9c6d9c5eadd0a440130d28103e6486303ecb8
Andrew Marder <andrew.ei.marder@gmail.com>
2011-08-24 14:28:28 -0700

    Added support for output ref in labels.

--- pyxform/utils.py
+++ pyxform/utils.py
@@ -1,4 +1,4 @@
-from xml.dom.minidom import Text, Element
+from xml.dom.minidom import Text, Element, parseString
 import re
 import codecs
 import json
@@ -8,31 +8,53 @@ SEP = "_"
 # http://www.w3.org/TR/REC-xml/
 TAG_START_CHAR = r"[a-zA-Z:_]"
 TAG_CHAR = r"[a-zA-Z:_0-9\-.]"
-XFORM_TAG_REGEXP = "%(start)s%(char)s*" % {"start" : TAG_START_CHAR, "char" : TAG_CHAR}
+XFORM_TAG_REGEXP = "%(start)s%(char)s*" % {
+    "start": TAG_START_CHAR,
+    "char": TAG_CHAR
+    }
+
 
 def is_valid_xml_tag(tag):
     return re.search(r"^" + XFORM_TAG_REGEXP + r"$", tag)
 
-def is_string(x):
-    return isinstance(x, basestring)
 
 def node(tag, *args, **kwargs):
     result = Element(tag)
+
+    unicode_args = [u for u in args if type(u) == unicode]
+    assert len(unicode_args) <= 1
+
+    parsedString = False
     for k, v in kwargs.iteritems():
-        result.setAttribute(k, v)
-    unicode_args = [u for u in args if type(u)==unicode]
-    assert len(unicode_args)<=1
-    if len(unicode_args)==1:
+        if k == 'toParseString':
+            if v == True and len(unicode_args) == 1:
+                parsedString = True
+                s = u'<?xml version="1.0" ?><'+tag+'>' + unicode_args[0] + u'</'+tag+'>'
+                node = parseString(s.encode("utf-8")).documentElement
+                childNodes = []
+                for child in node.childNodes:
+                    childNodes.append(child)
+                for child in childNodes:
+                    result.appendChild(child)
+        else:
+            result.setAttribute(k, v)
+
+    if len(unicode_args) == 1 and not parsedString:
         text_node = Text()
         text_node.data = unicode_args[0]
         result.appendChild(text_node)
     for n in args:
-        if not is_string(n):
+        if type(n) == int or type(n) == float or type(n) == str:
+            text_node = Text()
+            text_node.data = unicode(n)
+            result.appendChild(text_node)
+        elif type(n) != unicode:
             try:
                 result.appendChild(n)
             except:
-                raise Exception("Could not appendChild: %s" % n)
+                raise Exception(type(n), n)
     return result
+
 
 def get_pyobj_from_json(str_or_path):
     """
