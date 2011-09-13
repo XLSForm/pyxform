75302249f25850f9a49e6ba9112abb6502e06693
Andrew Marder <andrew.ei.marder@gmail.com>
2011-09-13 07:18:36 -0700

    Added test for settings.

---
+++ pyxform/tests/settings_test.py
@@ -0,0 +1,12 @@
+from unittest import TestCase
+from pyxform.builder import create_survey_from_path
+import utils
+
+
+class SettingsTests(TestCase):
+
+    def test_settings(self):
+        path = utils.path_to_text_fixture("settings.xls")
+        survey = create_survey_from_path(path)
+        self.assertEqual(survey.id_string(), "new_id")
+        self.assertEqual(survey.get_title(), "My Survey")
