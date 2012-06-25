82f0cc38bfc2dcb95e941128ed3fd8749de8df30
Mark Johnston <mejymejy@gmail.com>
2012-06-25 17:42:13 -0400

    updated pkg info to include all necessary files

--- setup.py
+++ setup.py
@@ -6,10 +6,17 @@ setup(
      author='modilabs',
      author_email='info@modilabs.org',
      packages=['pyxform', 'pyxform.odk_validate'],
+     package_dir={'pyxform': 'pyxform'},
+     package_data={
+        'pyxform': [
+            'odk_validate/java_lib/ODK Validate.jar',
+            'odk_validate/java_lib/lonely_java_src/FormValidator.java',
+        ],
+     },
      url='http://pypi.python.org/pypi/pyxform/',
      description='A Python package to create XForms for ODK Collect.',
      long_description=open('README.rst', 'rt').read(),
      install_requires=[
         'modilabs-python-utils==0.1.5',
-    ]
+    ],
 )
