d2d0c8f540dcfd2b2c1d461c536cbf63863498da
Nathan Breit <nabreit@gmail.com>
2012-07-26 17:33:03 -0700

    Updating spec test so it uses a output folder and a parallel expected output folder. The idea being that we can update the expected output by replacing it with a copy of the output folder if there is nothing unexpected in the diff.

--- pyxform/tests/xlsform_spec_test.py
+++ pyxform/tests/xlsform_spec_test.py
@@ -1,45 +1,41 @@
 """
 Some tests for the new (v0.9) spec is properly implemented.  
 """
-
 from unittest2 import TestCase
 import pyxform
 from pyxform import xls2json
-import os, sys
-import utils
-import codecs
+import os, codecs
+
+DIR = os.path.dirname(__file__)
 
 class main_test(TestCase):
     
     maxDiff = None
     
     def runTest(self):
-       
-        path_to_excel_file = utils.path_to_text_fixture("xlsform_spec_test.xls")
-        
+        filename = "xlsform_spec_test.xls"
+        path_to_excel_file = os.path.join(DIR, "example_xls", filename)
         #Get the xform output path:
-        directory, filename = os.path.split(path_to_excel_file)
         root_filename, ext = os.path.splitext(filename)
-        path_to_xform = os.path.join(directory, root_filename + ".xml")
-
+        output_path = os.path.join(DIR, "test_output", root_filename + ".xml")
+        expected_output_path = os.path.join(DIR, "test_expected_output", root_filename + ".xml")
         #Do the conversion:
         json_survey = xls2json.parse_file_to_json(path_to_excel_file)
         survey = pyxform.create_survey_element_from_dict(json_survey)
-        survey.print_xform_to_file(path_to_xform)
+        survey.print_xform_to_file(output_path)
         
         #Compare with the expected output:
-        expected_path = utils.path_to_text_fixture("spec_test_expected_output.xml")
-        with codecs.open(expected_path, 'rb', encoding="utf-8") as expected_file:
-            self.assertMultiLineEqual(expected_file.read(), survey.to_xml())
+        with codecs.open(expected_output_path, 'rb', encoding="utf-8") as expected_file:
+            with codecs.open(output_path, 'rb', encoding="utf-8") as actual_file:
+                self.assertMultiLineEqual(expected_file.read(), actual_file.read())
 
 class warnings_test(TestCase):
     """
     Just checks that the number of warnings thrown when reading warnings.xls doesn't change.
     """
     def runTest(self):
-        
-        path_to_excel_file = utils.path_to_text_fixture("warnings.xls")
-        
+        filename = "warnings.xls"
+        path_to_excel_file = os.path.join(DIR, "example_xls", filename)
         warnings = []
         xls2json.parse_file_to_json(path_to_excel_file, warnings=warnings)
         #print '\n'.join(warnings)
