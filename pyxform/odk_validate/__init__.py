5b37b6777715d2812fa3d1cb8bfc45260fdfedac
Nathan Breit <nabreit@gmail.com>
2012-08-06 19:47:58 -0700

    Working on better integration with ODK Validate...

--- pyxform/odk_validate/__init__.py
+++ pyxform/odk_validate/__init__.py
@@ -1,56 +1,58 @@
+"""
+odk_validate.py
+A python wrapper around ODK Validate
+"""
 from subprocess import Popen, PIPE
 import os, re, sys
-from collections import defaultdict
-# this is ugly, but allows running pyxform
-# as a standalone xls validator from the cl
-try:
-    from pyxform.errors import ValidationError
-except ImportError:
-    from errors import ValidationError
 
+import time
+import threading
+import signal
 
 CURRENT_DIRECTORY = os.path.dirname(os.path.realpath(__file__))
-ODK_VALIDATE_JAR = os.path.join(CURRENT_DIRECTORY, "java_lib", "ODK Validate.jar")
+ODK_VALIDATE_JAR = os.path.join(CURRENT_DIRECTORY, "ODK_Validate.jar")
 
-# Only the modded headless version of ODK Validate returns these codes.
-# see: lonely_java_src/FormValidator.java
-HEADLESS_ODK_VALIDATE_REGEXS = {
-    'result': r"^Result: (.*)$",
-    'error': r"^Error: (.*)$",
-    }
+#Adapted from:
+#http://betabug.ch/blogs/ch-athens/1093
+def run_popen_with_timeout(command, timeout):
+    """
+    Run a sub-program in subprocess.Popen, pass it the input_data,
+    kill it if the specified timeout has passed.
+    returns a tuple of success, stdout, stderr
+    """
+    print 'a'
+    kill_check = threading.Event()
+    def _kill_process_after_a_timeout(pid):
+        os.kill(pid, signal.SIGTERM)
+        kill_check.set() # tell the main routine that we had to kill
+        # use SIGKILL if hard to kill...
+        return
+    p = Popen(command, stdin=PIPE, stdout=PIPE, stderr=PIPE)
+    pid = p.pid
+    watchdog = threading.Timer(timeout, _kill_process_after_a_timeout, args=(pid, ))
+    watchdog.start()
+    (stdout, stderr) = p.communicate()
+    watchdog.cancel() # if it's still waiting to run
+    timeout = kill_check.isSet()
+    kill_check.clear()
+    return (p.returncode, timeout, stdout, stderr)
 
-
-class XFormValidator(object):
-
-    def _run_odk_validate(self, path_to_xform):
-        self._path_to_xform = path_to_xform
-        stdout, stderr = Popen(["java", "-jar", ODK_VALIDATE_JAR, self._path_to_xform], stdout=PIPE, stderr=PIPE).communicate()
-        self._odk_validate_output = (stdout + stderr).split('\n')
-
-    def get_odk_validate_output(self):
-        return "\n".join(self._odk_validate_output)
-
-    def _parse_odk_validate_output(self):
-        self._errors_and_result = defaultdict(list)
-        for line in self._odk_validate_output:
-            for key, regexp in HEADLESS_ODK_VALIDATE_REGEXS.items():
-                m = re.search(regexp, line)
-                if m:
-                    self._errors_and_result[key].append(m.group(1))
-
-    def is_valid(self):
-        return self._errors_and_result['result']==['Valid']
-
-    def validate(self, path_to_xform):
-        self._run_odk_validate(path_to_xform)
-        self._parse_odk_validate_output()
-        if not self.is_valid():
-            raise ValidationError(self.get_odk_validate_output())
-
-
-def check_xform(path):
-    validator = XFormValidator()
-    validator.validate(path)
+def check_xform(path_to_xform):
+    print ODK_VALIDATE_JAR
+    returncode, timeout, stdout, stderr = run_popen_with_timeout(["java", "-jar", ODK_VALIDATE_JAR, path_to_xform], 60)
+    #I could possibly do more to parse the output from validate.
+    #warnings = (stdout + stderr).split('\n\n')
+    #Consider leaving out stdout because it has a bunch of errors about unrecognized attributes that will always appear.
+    warnings = ['stderr:\n' + stderr] #['stdout:\n' + stdout] + 
+    if timeout:
+        warnings.append("XForm took to long to completely validate.")
+    if returncode > 0: #Error invalid
+        raise Exception('\n\n'.join(warnings))
+    if returncode == 0:
+        return warnings
+    if returncode < 0:
+        return ["Validate process incomplete. This error shouldn't happen."] + warnings
 
 if __name__ == '__main__':
+    print __doc__
     check_xform(sys.argv[1])
