98ca73b8ff82bc692ab581d787db5e8ae5b924ad
Ukang'a Dickson <ukanga@gmail.com>
2013-01-24 18:18:07 +0300

    bump version to 0.9.9.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
      name='pyxform',
-     version='0.9.9',
+     version='0.9.9.1',
      author='modilabs',
      author_email='info@modilabs.org',
      packages=['pyxform', 'pyxform.odk_validate'],
