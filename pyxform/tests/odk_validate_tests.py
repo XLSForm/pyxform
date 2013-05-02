83e93bf9a8cf06b36083ea3f3ec4a24546b88d7d
Ukang'a Dickson <ukanga@gmail.com>
2013-05-02 10:11:09 +0300

    add test for cleaner error message

--- pyxform/tests/odk_validate_tests.py
+++ pyxform/tests/odk_validate_tests.py
@@ -26,4 +26,33 @@ WARNING: choice value [Internationalcalltariffsareattractive] is too long"""
 Problem found at nodeset: """
         expectedStr += """/html/body/select1[@ref=${test1}]/item/value
     With element <value>"""
-        self.assertEqual(_cleanup_errors(testStr), expectedStr)
+        self.assertEqual(_cleanup_errors(testStr), expectedStr.strip())
+
+    def test_do_not_over_trim_javarosa_errors(self):
+        testStr = """
+org.javarosa.xpath.XPathUnhandledException: XPath evaluation: cannot handle function 'testfunc'
+org.javarosa.xpath.XPathUnhandledException: XPath evaluation: cannot handle function 'testfunc'
+\tat org.javarosa.xpath.expr.XPathFuncExpr.eval(XPathFuncExpr.java:270)
+\tat org.javarosa.xpath.XPathConditional.evalRaw(XPathConditional.java:68)
+\tat org.javarosa.core.model.condition.Recalculate.eval(Recalculate.java:53)
+\tat org.javarosa.core.model.condition.Triggerable.apply(Triggerable.java:69)
+\tat org.javarosa.core.model.FormDef.evaluateTriggerable(FormDef.java:706)
+\tat org.javarosa.core.model.FormDef.evaluateTriggerables(FormDef.java:696)
+\tat org.javarosa.core.model.FormDef.initializeTriggerables(FormDef.java:651)
+\tat org.javarosa.core.model.FormDef.initializeTriggerables(FormDef.java:629)
+\tat org.javarosa.core.model.FormDef.initialize(FormDef.java:1123)
+\tat org.odk.validate.FormValidator.validate(FormValidator.java:336)
+\tat org.odk.validate.FormValidator.<init>(FormValidator.java:95)
+\tat org.odk.validate.FormValidator.main(FormValidator.java:82)
+\tat sun.reflect.NativeMethodAccessorImpl.invoke0(Native Method)
+\tat sun.reflect.NativeMethodAccessorImpl.invoke(NativeMethodAccessorImpl.java:39)
+\tat sun.reflect.DelegatingMethodAccessorImpl.invoke(DelegatingMethodAccessorImpl.java:25)
+\tat java.lang.reflect.Method.invoke(Method.java:597)
+\tat org.eclipse.jdt.internal.jarinjarloader.JarRsrcLoader.main(JarRsrcLoader.java:58)
+>> Something broke the parser. See above for a hint.
+Result: Invalid"""
+        expectedStr = """
+XPath evaluation: cannot handle function 'testfunc'
+>> Something broke the parser. See above for a hint.
+Result: Invalid"""
+        self.assertEqual(_cleanup_errors(testStr), expectedStr.strip())
