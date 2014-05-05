1c750a084396ea046f0cfb072b579283b31c66a8
Ukang'a Dickson <ukanga@gmail.com>
2014-05-05 14:51:48 +0300

    add test for search and select function

--- pyxform/tests/xlsform_spec_test.py
+++ pyxform/tests/xlsform_spec_test.py
@@ -156,5 +156,36 @@ class PullDataTest(unittest.TestCase):
         os.remove(output_path)
 
 
+class SeachAndSelectTest(unittest.TestCase):
+
+    maxDiff = None
+
+    def runTest(self):
+        filename = "search_and_select.xlsx"
+        path_to_excel_file = os.path.join(DIR, "example_xls", filename)
+        # Get the xform output path:
+        root_filename, ext = os.path.splitext(filename)
+        output_path = os.path.join(DIR, "test_output", root_filename + ".xml")
+        expected_output_path = os.path.join(DIR, "test_expected_output",
+                                            root_filename + ".xml")
+        # Do the conversion:
+        warnings = []
+        json_survey = pyxform.xls2json.parse_file_to_json(
+            path_to_excel_file, warnings=warnings)
+        survey = pyxform.create_survey_element_from_dict(json_survey)
+        survey.print_xform_to_file(output_path, warnings=warnings)
+
+        # Compare with the expected output:
+        with codecs.open(expected_output_path, 'rb', encoding="utf-8") \
+                as expected_file:
+            with codecs.open(output_path, 'rb', encoding="utf-8") \
+                    as actual_file:
+                self.assertMultiLineEqual(
+                    expected_file.read(), actual_file.read())
+
+        # cleanup
+        os.remove(output_path)
+
+
 if __name__ == '__main__':
     unittest.main()
