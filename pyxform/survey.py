f1a9b0327d34add504ab60a21e2b3d958f2810d9
Ukang'a Dickson <ukanga@gmail.com>
2014-12-24 15:59:14 +0300

    remove trailing slash from xforms namespace, closes #14

--- pyxform/survey.py
+++ pyxform/survey.py
@@ -19,7 +19,7 @@ nsmap = {
     u"xmlns:ev": u"http://www.w3.org/2001/xml-events",
     u"xmlns:xsd": u"http://www.w3.org/2001/XMLSchema",
     u"xmlns:jr": u"http://openrosa.org/javarosa",
-    u"xmlns:orx": u"http://openrosa.org/xforms/"
+    u"xmlns:orx": u"http://openrosa.org/xforms"
     }
 
 class Survey(Section):
