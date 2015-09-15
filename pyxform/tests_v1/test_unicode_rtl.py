cde6b323f96df71b4746ad471c58f8b597bfb78b
Alex Dorey <dorey415@gmail.com>
2015-09-15 18:49:45 -0400

    Test handling of mixed-direction text - #47
    Set out to write a test that fails because of #47 but couldn't verify
    the incorrect output. (lines 31 and 45 may need to be updated)

--- pyxform/tests_v1/test_unicode_rtl.py
+++ pyxform/tests_v1/test_unicode_rtl.py
@@ -17,3 +17,31 @@ class UnicodeRtl(PyxformTestCase):
                 '<label>☃</label>',
             ],
         )
+
+    def test_mixed_rtl(self):
+        self.assertPyxformXform(
+            name='mixed_rtl_test',
+            md="""
+            | survey |      |            |                      |
+            |        | type | name       | label                |
+            |        | text | q1         | Q1                   |
+            |        | text | urdu_mixed | Q6. صوبہ ؟ ${q1} کیا |
+            """,
+            xml__contains=[
+                '<label>Q6. صوبہ ؟ <output value=" /mixed_rtl_test/q1 "/> کیا</label>',
+            ]
+        )
+
+    def test_mixed_rtl_multilang(self):
+        self.assertPyxformXform(
+            name='mixed_rtl_test',
+            md="""
+            | survey |      |            |                      |            |
+            |        | type | name       | label::urdu          | label::eng |
+            |        | text | q1         | Question 1           | Q1         |
+            |        | text | urdu_mixed | Q6. صوبہ ؟ ${q1} کیا | Q2         |
+            """,
+            itext__contains=[
+                '<value>Q6. صوبہ ؟ <output value=" /mixed_rtl_test/q1 "/> کیا</value>',
+            ]
+        )
