896956b713cb282d0f4f3663846ec2dafb6bf28d
Alex Dorey <dorey415@gmail.com>
2015-12-16 16:30:14 -0500

    move "missing headers" test into new format

---
+++ pyxform/tests_v1/test_bug_missing_headers.py
@@ -0,0 +1,33 @@
+from pyxform_test_case import PyxformTestCase
+
+
+class MissingHeaders(PyxformTestCase):
+    '''
+    when survey and choices columns are missing headers, it is helpful to see
+    an error message that prompts the user to include necessary headers.
+    '''
+    def test_missing_survey_headers(self):
+        self.assertPyxformXform(
+            md="""
+            | survey |                 |    |
+            |        | select_one list | S1 |
+            """,
+            errored=True,
+            error__contains=[
+                'missing important column headers',
+            ])
+
+    def test_missing_choice_headers(self):
+        self.assertPyxformXform(
+            md="""
+            | survey  |                 |          |      |
+            |         | type            | label    | name |
+            |         | select_one list | S1       | s1   |
+            | choices |                 |          |      |
+            |         | list            | option a | a    |
+            |         | list            | option b | b    |
+            """,
+            errored=True,
+            error__contains=[
+                "has columns 'list name', 'name', and 'label'",
+            ])
