0c9a9582970b9405121e88e293fa24b81bd36773
S. Andrew Sheppard <andrew@wq.io>
2016-01-25 00:25:46 -0500

    use setuptools to run tests
    see https://nose.readthedocs.org/en/latest/#python3

--- setup.py
+++ setup.py
@@ -1,16 +1,21 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
 
 setup(
     name='pyxform',
     version='0.9.22',
     author='modilabs',
     author_email='info@modilabs.org',
-    packages=['pyxform', 'pyxform.odk_validate'],
-    package_dir={'pyxform': 'pyxform'},
+    packages=find_packages(),
     package_data={
-        'pyxform': [
-            'odk_validate/ODK_Validate.jar',
+        'pyxform.odk_validate': [
+            'ODK_Validate.jar',
         ],
+        'pyxform.tests': [
+            'example_xls/*.*',
+            'bug_example_xls/*.*',
+            'test_output/*.*',
+            'test_expected_output/*.*',
+        ]
     },
     url='http://pypi.python.org/pypi/pyxform/',
     description='A Python package to create XForms for ODK Collect.',
