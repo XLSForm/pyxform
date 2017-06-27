15ab835eb479651961a1c630812c49d6412c0093
Njagi Mwaniki <njagi@urbanslug.com>
2017-06-27 17:02:26 +0300

    Reimplemented test_osm using markdown tests
    Signed-off-by: Njagi Mwaniki <njagi@urbanslug.com>

---
+++ pyxform/tests_v1/test_osm.py
@@ -0,0 +1,55 @@
+from pyxform.tests_v1.pyxform_test_case import PyxformTestCase
+
+
+class OSMWidgetsTest(PyxformTestCase):
+    def test_osm_type(self):
+        self.assertPyxformXform(
+            name="osm",
+            debug=True,
+            md="""
+            | survey |                   |              |          |
+            |        | type              |   name       | label    |
+            |        | osm               | osm_road     | Road     |
+            |        | osm building_tags | osm_building | Building |
+            | osm    |                   |              |          |
+            |        | list name         |  name        | label    |
+            |        | building_tags     | name         | Name     |
+            |        | building_tags     | addr:city    | City     |
+
+            """,
+            xml__contains=[
+                """
+  <h:head>
+    <h:title>pyxform_autotesttitle</h:title>
+    <model>
+      <instance>
+        <osm id="pyxform_autotest_id_string">
+          <osm_road/>
+          <osm_building/>
+          <meta>
+            <instanceID/>
+          </meta>
+        </osm>
+      </instance>
+      <bind nodeset="/osm/osm_road" type="binary"/>
+      <bind nodeset="/osm/osm_building" type="binary"/>
+      <bind calculate="concat('uuid:', uuid())" nodeset="/osm/meta/instanceID" readonly="true()" type="string"/>
+    </model>
+  </h:head>
+  <h:body>
+    <upload mediatype="osm/*" ref="/osm/osm_road">
+      <label>Road</label>
+    </upload>
+    <upload mediatype="osm/*" ref="/osm/osm_building">
+      <label>Building</label>
+      <tag key="name">
+        <label>Name</label>
+      </tag>
+      <tag key="addr:city">
+        <label>City</label>
+      </tag>
+    </upload>
+  </h:body>
+</h:html>"""
+            ],
+        )
