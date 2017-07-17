76e436daef154bdf6acd1b0569f6fa2baa61addd
lognaturel <lognaturel@gmail.com>
2017-07-17 10:21:30 -0700

    Add test for blank audit name.

--- pyxform/tests_v1/test_audit.py
+++ pyxform/tests_v1/test_audit.py
@@ -31,3 +31,18 @@ class AuditTest(PyxformTestCase):
                 '</meta>',
                 '<bind nodeset="/meta_audit/meta/audit" type="binary"/>'],
         )
+
+    def test_audit_blank_name(self):
+        self.assertPyxformXform(
+            name="meta_audit",
+            md="""
+            | survey |        |          |       |
+            |        | type   |   name   | label |
+            |        | audit  |          |       |
+            """,
+            xml__contains=[
+                '<meta>',
+                '<audit/>',
+                '</meta>',
+                '<bind nodeset="/meta_audit/meta/audit" type="binary"/>'],
+        )
