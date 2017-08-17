928b5022f2041acdb8d0b4b871718c16f01ec677
Alex Anderson <anderson@medicmobile.org>
2017-08-17 19:20:27 +0000

    Add tests demonstrating current behaviour

--- pyxform/tests_v1/test_whitespace.py
+++ pyxform/tests_v1/test_whitespace.py
@@ -1,15 +1,41 @@
 from pyxform.tests_v1.pyxform_test_case import PyxformTestCase
 
 class WhitespaceTest(PyxformTestCase):
-    def test_over_trim(self):
+    def test_over_trim_before(self):
         self.assertPyxformXform(
             name='issue96',
             md="""
-            | survey  |                 |             |       |
-            |         | type            | label       | name  |
-            |         | text            | Ignored     | var   |
-            |         | note            | ${var} text | label |
+            | survey  |                 |            |       |
+            |         | type            | label      | name  |
+            |         | text            | Ignored    | var   |
+            |         | note            | ${var} end | label |
             """,
             xml__contains=[
-                '<label><output value=" /issue96/var "/> text </label>',
+                '<label><output value=" /issue96/var "/> end </label>',
+            ])
+
+    def test_over_trim_middle(self):
+        self.assertPyxformXform(
+            name='issue96',
+            md="""
+            | survey  |                 |                  |       |
+            |         | type            | label            | name  |
+            |         | text            | Ignored          | var   |
+            |         | note            | start ${var} end | label |
+            """,
+            xml__contains=[
+                '<label> start <output value=" /issue96/var "/> end </label>',
+            ])
+
+    def test_over_trim_after(self):
+        self.assertPyxformXform(
+            name='issue96',
+            md="""
+            | survey  |                 |              |       |
+            |         | type            | label        | name  |
+            |         | text            | Ignored      | var   |
+            |         | note            | start ${var} | label |
+            """,
+            xml__contains=[
+                '<label> start <output value=" /issue96/var "/> </label>',
             ])
