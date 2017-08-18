70df7d79296034fc3ace6d3b601dea08609c86a9
Alex Anderson <anderson@medicmobile.org>
2017-08-18 07:47:52 +0000

    Update tests to use ss_structure, showing observed behaviour

--- pyxform/tests_v1/test_whitespace.py
+++ pyxform/tests_v1/test_whitespace.py
@@ -3,39 +3,30 @@ from pyxform.tests_v1.pyxform_test_case import PyxformTestCase
 class WhitespaceTest(PyxformTestCase):
     def test_over_trim_before(self):
         self.assertPyxformXform(
-            name='issue96',
-            md="""
-            | survey  |                 |            |       |
-            |         | type            | label      | name  |
-            |         | text            | Ignored    | var   |
-            |         | note            | ${var} end | label |
-            """,
+            name='issue96_issue146',
+            ss_structure={'survey': [
+                    { 'type':'text', 'label':'Ignored',    'name':'var'   },
+                    { 'type':'note', 'label':'${var} end', 'name':'label' } ] },
             xml__contains=[
-                '<label><output value=" /issue96/var "/> end</label>',
+                '<label><output value=" /issue96_issue146/var "/> end </label>',
             ])
 
     def test_over_trim_middle(self):
         self.assertPyxformXform(
-            name='issue96',
-            md="""
-            | survey  |                 |                  |       |
-            |         | type            | label            | name  |
-            |         | text            | Ignored          | var   |
-            |         | note            | start ${var} end | label |
-            """,
+            name='issue96_issue146',
+            ss_structure={'survey': [
+                    { 'type':'text', 'label':'Ignored',          'name':'var'   },
+                    { 'type':'note', 'label':'start ${var} end', 'name':'label' } ] },
             xml__contains=[
-                '<label>start <output value=" /issue96/var "/> end</label>',
+                '<label> start <output value=" /issue96_issue146/var "/> end </label>',
             ])
 
     def test_over_trim_after(self):
         self.assertPyxformXform(
-            name='issue96',
-            md="""
-            | survey  |                 |              |       |
-            |         | type            | label        | name  |
-            |         | text            | Ignored      | var   |
-            |         | note            | start ${var} | label |
-            """,
+            name='issue96_issue146',
+            ss_structure={'survey': [
+                    { 'type':'text', 'label':'Ignored',      'name':'var'   },
+                    { 'type':'note', 'label':'start ${var}', 'name':'label' } ] },
             xml__contains=[
-                '<label>start <output value=" /issue96/var "/></label>',
+                '<label> start <output value=" /issue96_issue146/var "/> </label>',
             ])
