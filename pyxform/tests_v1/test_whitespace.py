ce72e9167542eb944a9e2b23e1f1b94596b9c6bc
Alex Anderson <anderson@medicmobile.org>
2017-08-18 07:49:08 +0000

    Update tests with expected behaviour

--- pyxform/tests_v1/test_whitespace.py
+++ pyxform/tests_v1/test_whitespace.py
@@ -8,7 +8,7 @@ class WhitespaceTest(PyxformTestCase):
                     { 'type':'text', 'label':'Ignored',    'name':'var'   },
                     { 'type':'note', 'label':'${var} end', 'name':'label' } ] },
             xml__contains=[
-                '<label><output value=" /issue96_issue146/var "/> end </label>',
+                '<label><output value=" /issue96_issue146/var "/> end</label>',
             ])
 
     def test_over_trim_middle(self):
@@ -18,7 +18,7 @@ class WhitespaceTest(PyxformTestCase):
                     { 'type':'text', 'label':'Ignored',          'name':'var'   },
                     { 'type':'note', 'label':'start ${var} end', 'name':'label' } ] },
             xml__contains=[
-                '<label> start <output value=" /issue96_issue146/var "/> end </label>',
+                '<label>start <output value=" /issue96_issue146/var "/> end</label>',
             ])
 
     def test_over_trim_after(self):
@@ -28,5 +28,5 @@ class WhitespaceTest(PyxformTestCase):
                     { 'type':'text', 'label':'Ignored',      'name':'var'   },
                     { 'type':'note', 'label':'start ${var}', 'name':'label' } ] },
             xml__contains=[
-                '<label> start <output value=" /issue96_issue146/var "/> </label>',
+                '<label>start <output value=" /issue96_issue146/var "/></label>',
             ])
