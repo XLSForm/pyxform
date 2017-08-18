4403dfd6723c2e9a52112a2ff5da2188d9a5e996
Alex Anderson <anderson@medicmobile.org>
2017-08-18 08:25:36 +0000

    Fix whitespace trimming

--- pyxform/survey.py
+++ pyxform/survey.py
@@ -428,12 +428,21 @@ class Survey(Section):
         # TODO: check out pyxml
         # http://ronrothman.com/public/leftbraned/xml-dom-minidom-toprettyxml-and-silly-whitespace/
         xml_with_linebreaks = self.xml().toprettyxml(indent='  ')
-        text_re = re.compile('(>)\n\s*(\s[^<>\s].*?)\n\s*(\s</)', re.DOTALL)
-        output_re = re.compile('\n.*(<output.*>)\n(  )*')
-        pretty_xml = text_re.sub(lambda m: ''.join(m.group(1, 2, 3)), xml_with_linebreaks)
-        inline_output = output_re.sub('\g<1>', pretty_xml)
+
+        # Trim whitespace from text nodes
+        inline_output = re.compile('([^/]>)\n\s*([^<>\s].*?\n\s*</)', re.DOTALL)\
+            .sub('\g<1>\g<2>', xml_with_linebreaks)
+        inline_output = re.compile('(>\n\s*[^<>\s].*?)\n\s*</', re.DOTALL)\
+            .sub('\g<1></', inline_output)
+
+        # Trim linebreaks around output nodes
+        inline_output = re.compile('\n.*(<output.*>)\n(  )*')\
+            .sub('\g<1>', inline_output)
+
+        # Remove whitespace from empty labels
         inline_output = re.compile('<label>\s*\n*\s*\n*\s*</label>')\
             .sub('<label></label>', inline_output)
+
         return '<?xml version="1.0"?>\n' + inline_output
 
     def __repr__(self):
