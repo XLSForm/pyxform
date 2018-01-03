952a940d8abf31742e98940551fbdc06c5642987
lindsay stevens <lindsay.stevens.au@gmail.com>
2018-01-03 23:25:52 +1100

    add: skip enketo-validate test cases if node not installed
    - should not be a hard dependency for dev / running tests locally,
      since it's an optional validator.

--- pyxform/tests/enketo_validate_tests.py
+++ pyxform/tests/enketo_validate_tests.py
@@ -1,9 +1,10 @@
-from unittest2 import TestCase
+from unittest2 import TestCase, skipIf
 
 from pyxform.tests.utils import prep_class_config
-from pyxform.validators.enketo_validate import check_xform
+from pyxform.validators.enketo_validate import check_xform, _node_installed
 
 
+@skipIf(not _node_installed(), "Only run if node installed.")
 class EnketoValidateTests(TestCase):
     maxDiff = None
 
