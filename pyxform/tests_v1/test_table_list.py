74be6e1c73e0e8e7090f76e6c78c3a2c38a2f06b
vince <vince.buscarello@gmail.com>
2018-12-15 18:01:12 -0700

    more detailed test

--- pyxform/tests_v1/test_table_list.py
+++ pyxform/tests_v1/test_table_list.py
@@ -2,28 +2,45 @@ from pyxform.tests_v1.pyxform_test_case import PyxformTestCase
 
 
 MD = '''
-| survey  |                    |            |       |                   |     
-|         | type               | name       | label |appearance         |
-|         | begin_group        | tablelist1 | Table |table-list minimal |
-|         | select_one yes_no  | options1a  | Q1    |                   |                             
-|         | select_one yes_no  | options1b  | Q2    |                   |
-|         | end_group          |            |       |                   |
-| choices |                    |            |       |                   |
-|         | list_name          | name       | label |                   |
-|         | yes_no             | yes        | Yes   |                   |
-|         | yes_no             | no         | No    |                   |
+| survey  |                    |            |           |            |                   |
+|         | type               | name       | label     | hint       |appearance         |
+|         | begin_group        | tablelist1 | Table_Y_N |            |table-list minimal |
+|         | select_one yes_no  | options1a  | Q1        | first row! |                   |
+|         | select_one yes_no  | options1b  | Q2        |            |                   |
+|         | end_group          |            |           |            |                   |
+| choices |                    |            |           |            |                   |
+|         | list_name          | name       | label     |            |                   |
+|         | yes_no             | yes        | Yes       |            |                   |
+|         | yes_no             | no         | No        |            |                   |
             """  # noqa
 '''  # nopep8
 
 XML_CONTAINS = '''
-<group appearance="field-list minimal" 
+    <group appearance="field-list minimal" ref="/table-list-appearance-mod/tablelist1">
+      <input ref="/table-list-appearance-mod/tablelist1/generated_table_list_label_2">
+        <label>Table_Y_N</label>
+      </input>
+      <select1 appearance="label" ref="/table-list-appearance-mod/tablelist1/reserved_name_for_field_list_labels_3">
+        <label> </label>
+        <item>
+          <label>Yes</label>
+          <value>yes</value>
+        </item>
+        <item>
+          <label>No</label>
+          <value>no</value>
+        </item>
+      </select1>
+      <select1 appearance="list-nolabel" ref="/table-list-appearance-mod/tablelist1/options1a">
+        <label>Q1</label>
+        <hint>first row!</hint>
 '''.strip()  # nopep8
 
 
 class AreaTest(PyxformTestCase):
     def test_area(self):
         self.assertPyxformXform(
-            name="area",
+            name="table-list-appearance-mod",
             md=MD,
             xml__contains=[XML_CONTAINS],
             debug=True
@@ -32,7 +49,8 @@ class AreaTest(PyxformTestCase):
 """ 
 xls
 
-begin group	happy_sad_table	Table with image labels (made using an easier method)	This is a user-defined hint for the 1st row							table-list minimal
+begin group	happy_sad_table	Table with image labels (made using an easier method)
+This is a user-defined hint for the 1st row							table-list minimal
 select_multiple  happy_sad	happy_sad_brian	Brian	This is a user-defined hint for the 2nd row							
 select_multiple happy_sad	happy_sad_michael	Michael	This is a user-defined hint for the 3rd row							
 end group										
