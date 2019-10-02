e1cdcf4b3483700e1a2f392ef4eb0584e416a0cb
Nyoman Ribeka <nyoman.ribeka@gmail.com>
2019-10-02 15:48:10 -0400

    Updating and adding unit tests to test unicode.

--- pyxform/tests_v1/pyxform_test_case.py
+++ pyxform/tests_v1/pyxform_test_case.py
@@ -219,7 +219,7 @@ class PyxformTestCase(PyxformMarkdown, TestCase):
                 )
             for v_err in odk_validate_error__contains:
                 self.assertContains(
-                    e.args[0].decode("utf-8"),
+                    e.args[0],
                     v_err,
                     msg_prefix="odk_validate_error__contains",
                 )
