f201532881e3b7724137dba6ade80d9a41df256e
Yaw Anokwa <yanokwa@gmail.com>
2019-12-20 10:25:59 -0800

    Cleaner formatting

--- pyxform/question_type_dictionary.py
+++ pyxform/question_type_dictionary.py
@@ -11,7 +11,7 @@ def generate_new_dict():
     type dictionary from all.xls again.
     It shouldn't be called as part of any application.
     """
-    path_to_question_types = ( "/pyxform/question_types/all.xls" )
+    path_to_question_types = "/pyxform/question_types/all.xls"
     json_dict = QuestionTypesReader(path_to_question_types).to_json_dict()
     print_pyobj_to_json(json_dict, "new_question_type_dict.json")
 
