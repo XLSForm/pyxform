bddf278fc4b0189e8517268f51050602ca7370a6
Yaw Anokwa <yanokwa@users.noreply.github.com>
2020-02-06 13:36:32 -0800

    Prepare for v1.0.0 release (#421)

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ REQUIRES = [
 
 setup(
     name="pyxform",
-    version="0.15.1",
+    version="1.0.0",
     author="github.com/xlsform",
     author_email="info@xlsform.org",
     packages=find_packages(),
