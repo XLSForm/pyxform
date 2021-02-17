bdf35f5d45bd701bc720cd7bed6db5d7b311e713
Callum Stott <callum@seadowg.com>
2021-02-17 16:04:32 +0000

    Add ignored test for recordaction validation

--- pyxform/tests_v1/test_background_audio.py
+++ pyxform/tests_v1/test_background_audio.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
 from pyxform.tests_v1.pyxform_test_case import PyxformTestCase
+import unittest
 
 
-class AuditTest(PyxformTestCase):
+class BackgroundAudioTest(PyxformTestCase):
     def test_background_audio(self):
         self.assertPyxformXform(
             name="data",
@@ -14,4 +15,16 @@ class AuditTest(PyxformTestCase):
             xml__contains=[
                 '<odk:recordaudio event="odk-instance-load" ref="/data/my_recording"/>',
             ],
+        )
+
+    @unittest.skip("Required update to Validate to work")
+    def test_background_audio_is_valid(self):
+        self.assertPyxformXform(
+            name="data",
+            md="""
+            | survey |                   |                 |
+            |        | type              |   name          |
+            |        | background-audio  |   my_recording  |
+            """,
+            run_odk_validate=True,
         )
