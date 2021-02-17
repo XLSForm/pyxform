6a7b49dd5c36cb36f972a30e4ec73ccc97eea0de
Callum Stott <callum@seadowg.com>
2021-02-17 17:43:13 +0000

    Update Validate fore recordaudio support

--- pyxform/tests_v1/test_background_audio.py
+++ pyxform/tests_v1/test_background_audio.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 from pyxform.tests_v1.pyxform_test_case import PyxformTestCase
-import unittest
 
 
 class BackgroundAudioTest(PyxformTestCase):
@@ -78,16 +77,4 @@ class BackgroundAudioTest(PyxformTestCase):
             """,
             errored=True,
             error__contains=["Invalid value for quality."],
-        )
-
-    @unittest.skip("Required update to Validate to work")
-    def test_background_audio_is_valid(self):
-        self.assertPyxformXform(
-            name="data",
-            md="""
-            | survey |                   |                 |
-            |        | type              |   name          |
-            |        | background-audio  |   my_recording  |
-            """,
-            run_odk_validate=True,
         )
