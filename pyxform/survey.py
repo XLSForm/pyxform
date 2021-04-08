2e9fcf8ac0aa2c50394d1e6b12ca9c81346047b6
John N. Milner <john@tmoj.net>
2021-04-08 12:45:24 -0400

    Revert "Fixed failing old tests"
    This reverts commit 4f1536fe1601b9fcca3f25c6391c16937b12f2d4.

--- pyxform/survey.py
+++ pyxform/survey.py
@@ -187,7 +187,7 @@ class Survey(Section):
         root_node_name = self.name
         section_names = []
         for element in self.iter_descendants():
-            if not isinstance(element, Survey) and element.name == root_node_name:
+            if element.type != "survey" and element.name == root_node_name:
                 raise PyXFormError(
                     'The name "%s" is the same as the form name. '
                     "Use a different section name "
