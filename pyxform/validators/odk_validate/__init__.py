2b8e2ff7d416858e3e6a2e3ba0c1dd7432c42e0b
lindsay stevens <lindsay.stevens.au@gmail.com>
2021-06-09 20:19:25 +1000

    fix: remove java version check
    - incompatibility will surface like any other error e.g. pyxform/#257

--- pyxform/validators/odk_validate/__init__.py
+++ pyxform/validators/odk_validate/__init__.py
@@ -7,7 +7,6 @@ from __future__ import print_function, unicode_literals
 
 import logging
 import os
-import re
 import sys
 
 from pyxform.validators.error_cleaner import ErrorCleaner
@@ -53,47 +52,13 @@ def install_ok(bin_file_path=ODK_VALIDATE_PATH):
     return True
 
 
-def check_java_version():
-    """Check java version is greater than or equal to java 8.
-
-    Raises EnvironmentError exception if java version is less than java 8.
-    """
-    try:
-        stderr = str(
-            run_popen_with_timeout(
-                ["java", "-Djava.awt.headless=true", "-version"], 100
-            )[3]
-        )
-    except OSError as os_error:
-        stderr = str(os_error)
-    if "java version" not in stderr and "openjdk version" not in stderr:
-        raise EnvironmentError("pyxform odk validate dependency: java not found")
-    # extract version number from version string
-    java_version_str = stderr.split("\n")[0]
-    # version number is usually inside double-quotes.
-    # Using regex to find that in the string
-    java_version = re.findall(r"\"(.+?)\"", java_version_str)[0]
-    if "." in java_version:
-        major, minor = java_version.split(".")[0], java_version.split(".")[1]
-    elif "-" in java_version:
-        major, minor = java_version.split("-")[0], 0
-    else:
-        major, minor = java_version, 0
-    if not ((int(major) == 1 and int(minor) >= 8) or int(major) >= 8):
-        raise EnvironmentError(
-            "pyxform odk validate dependency: " "java 8 or newer version not found"
-        )
-
-
 def check_xform(path_to_xform):
     """Run ODK Validate against the XForm in `path_to_xform`.
 
     Returns an array of warnings if the form is valid.
     Throws an exception if it is not
+    Does not do a LBYL check for compatible java version as per pyxform/#481
     """
-    # check for available java version
-    check_java_version()
-
     # resultcode indicates validity of the form
     # timeout indicates whether validation ran out of time to complete
     # stdout is not used because it has some warnings that always
