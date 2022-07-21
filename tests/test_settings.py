df4e01e8cbb603b806a5a7245b4e1ac4e08091c9
lindsay stevens <lindsay.stevens.au@gmail.com>
2022-07-21 19:37:27 +1000

    fix: upgrade fixture-based test for instance_xmlns setting
    - The test was broken by changes to choices processing (#603).
    - The test conditions seem to relate only to the instance_xmlns feature.
    - The new test asserts the same using the pyxformtestcase pattern.

---
+++ tests/test_settings.py
@@ -0,0 +1,47 @@
+from tests.pyxform_test_case import PyxformTestCase
+
+
+class TestSettings(PyxformTestCase):
+
+    def test_instance_xmlns__is_set__custom_xmlns(self):
+        """Should find the instance_xmlns value in the instance xmlns attribute."""
+        md = """
+        | settings |
+        |          | instance_xmlns             |
+        |          | 1234 |
+        | survey |       |      |       |
+        |        | type  | name | label |
+        |        | text  | q1   | hello |
+        """
+        self.assertPyxformXform(
+            md=md,
+            xml__xpath_match=[
+                """
+                /h:html/h:head/x:model/x:instance/*[
+                  local-name()='test_name'
+                  and @id='test_id'
+                  and namespace-uri()='1234'
+                ]
+                """
+            ]
+        )
+
+    def test_instance_xmlns__not_set__default_xmlns(self):
+        """Should find the default xmlns for the instance element."""
+        md = """
+        | survey |       |      |       |
+        |        | type  | name | label |
+        |        | text  | q1   | hello |
+        """
+        self.assertPyxformXform(
+            md=md,
+            xml__xpath_match=[
+                """
+                /h:html/h:head/x:model/x:instance/*[
+                  local-name()='test_name'
+                  and @id='test_id'
+                  and namespace-uri()='http://www.w3.org/2002/xforms'
+                ]
+                """
+            ],
+        )
