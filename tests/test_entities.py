1618bfb1349d2ac4ec3593f0a68d6f27f2701bd6
Hélène Martin <lognaturel@gmail.com>
2023-01-17 11:53:40 -0800

    Combine basic entity test cases into single test

--- tests/test_entities.py
+++ tests/test_entities.py
@@ -3,9 +3,10 @@ from tests.pyxform_test_case import PyxformTestCase
 
 
 class EntitiesTest(PyxformTestCase):
-    def test_dataset_in_entities_sheet__adds_meta_entity_block(self):
+    def test_basic_entity_creation_building_blocks(self):
         self.assertPyxformXform(
             name="data",
+            debug=True,
             md="""
             | survey   |         |       |       |
             |          | type    | name  | label |
@@ -14,7 +15,19 @@ class EntitiesTest(PyxformTestCase):
             |          | dataset | label |       |
             |          | trees   | a     |       |
             """,
-            xml__xpath_match=["/h:html/h:head/x:model/x:instance/x:data/x:meta/x:entity"],
+            xml__xpath_match=[
+                "/h:html/h:head/x:model/x:instance/x:data/x:meta/x:entity",
+                '/h:html/h:head/x:model/x:instance/x:data/x:meta/x:entity[@dataset = "trees"]',
+                # defaults to always creating
+                '/h:html/h:head/x:model/x:instance/x:data/x:meta/x:entity[@create = "1"]',
+                '/h:html/h:head/x:model/x:instance/x:data/x:meta/x:entity[@id = ""]',
+                '/h:html/h:head/x:model/x:bind[@nodeset = "/data/meta/entity/@id" and @type = "string" and @readonly = "true()"]',
+                '/h:html/h:head/x:model/x:setvalue[@event = "odk-instance-first-load" and @type = "string" and @ref = "/data/meta/entity/@id" and @value = "uuid()"]',
+                "/h:html/h:head/x:model/x:instance/x:data/x:meta/x:entity/x:label",
+                '/h:html/h:head/x:model/x:bind[@nodeset = "/data/meta/entity/label" and @type = "string" and @readonly = "true()" and @calculate = "a"]',
+                '/h:html/h:head/x:model[@entities:entities-version = "2022.1.0"]',
+            ],
+            xml__contains=['xmlns:entities="http://www.opendatakit.org/xforms/entities"'],
         )
 
     def test_multiple_dataset_rows_in_entities_sheet__errors(self):
@@ -32,22 +45,6 @@ class EntitiesTest(PyxformTestCase):
             errored=True,
             error__contains=[
                 "This version of pyxform only supports declaring a single entity per form. Please make sure your entities sheet only declares one entity."
-            ],
-        )
-
-    def test_dataset_in_entities_sheet__adds_dataset_attribute_to_entity(self):
-        self.assertPyxformXform(
-            name="data",
-            md="""
-            | survey   |         |       |       |
-            |          | type    | name  | label |
-            |          | text    | a     | A     |
-            | entities |         |       |       |
-            |          | dataset | label |       |
-            |          | trees   | a     |       |
-            """,
-            xml__xpath_match=[
-                '/h:html/h:head/x:model/x:instance/x:data/x:meta/x:entity[@dataset = "trees"]'
             ],
         )
 
@@ -118,22 +115,6 @@ class EntitiesTest(PyxformTestCase):
             ],
         )
 
-    def test_dataset_in_entities_sheet__defaults_to_always_creating(self):
-        self.assertPyxformXform(
-            name="data",
-            md="""
-            | survey   |         |       |       |
-            |          | type    | name  | label |
-            |          | text    | a     | A     |
-            | entities |         |       |       |
-            |          | dataset | label |       |
-            |          | trees   | a     |       |
-            """,
-            xml__xpath_match=[
-                '/h:html/h:head/x:model/x:instance/x:data/x:meta/x:entity[@create = "1"]'
-            ],
-        )
-
     def test_create_if_in_entities_sheet__puts_expression_on_bind(self):
         self.assertPyxformXform(
             name="data",
@@ -148,41 +129,6 @@ class EntitiesTest(PyxformTestCase):
             xml__xpath_match=[
                 '/h:html/h:head/x:model/x:bind[@nodeset = "/data/meta/entity/@create" and @calculate = "string-length(a) > 3"]',
                 '/h:html/h:head/x:model/x:instance/x:data/x:meta/x:entity[@create = "1"]',
-            ],
-        )
-
-    def test_dataset_in_entities_sheet__adds_id_attribute_and_model_nodes_to_entity(self):
-        self.assertPyxformXform(
-            name="data",
-            md="""
-            | survey   |         |       |       |
-            |          | type    | name  | label |
-            |          | text    | a     | A     |
-            | entities |         |       |       |
-            |          | dataset | label |       |
-            |          | trees   | a     |       |
-            """,
-            xml__xpath_match=[
-                '/h:html/h:head/x:model/x:instance/x:data/x:meta/x:entity[@id = ""]',
-                '/h:html/h:head/x:model/x:bind[@nodeset = "/data/meta/entity/@id" and @type = "string" and @readonly = "true()"]',
-                '/h:html/h:head/x:model/x:setvalue[@event = "odk-instance-first-load" and @type = "string" and @ref = "/data/meta/entity/@id" and @value = "uuid()"]',
-            ],
-        )
-
-    def test_label_in_entities_sheet__adds_label_and_bind_to_entity(self):
-        self.assertPyxformXform(
-            name="data",
-            md="""
-            | survey   |         |       |       |
-            |          | type    | name  | label |
-            |          | text    | a     | A     |
-            | entities |         |       |       |
-            |          | dataset | label |       |
-            |          | trees   | a     |       |
-            """,
-            xml__xpath_match=[
-                "/h:html/h:head/x:model/x:instance/x:data/x:meta/x:entity/x:label",
-                '/h:html/h:head/x:model/x:bind[@nodeset = "/data/meta/entity/label" and @type = "string" and @readonly = "true()" and @calculate = "a"]',
             ],
         )
 
@@ -218,20 +164,6 @@ class EntitiesTest(PyxformTestCase):
             error__contains=["The entities sheet is missing the required label column."],
         )
 
-    def test_dataset_in_entities_sheet__adds_entities_namespace(self):
-        self.assertPyxformXform(
-            name="data",
-            md="""
-            | survey   |         |       |       |
-            |          | type    | name  | label |
-            |          | text    | a     | A     |
-            | entities |         |       |       |
-            |          | dataset | label |       |
-            |          | trees   | a     |       |
-            """,
-            xml__contains=['xmlns:entities="http://www.opendatakit.org/xforms/entities"'],
-        )
-
     def test_entities_namespace__omitted_if_no_entities_sheet(self):
         self.assertPyxformXform(
             name="data",
@@ -241,22 +173,6 @@ class EntitiesTest(PyxformTestCase):
             |          | text    | a     | A     |
             """,
             xml__excludes=['xmlns:entities="http://www.opendatakit.org/xforms/entities"'],
-        )
-
-    def test_dataset_in_entities_sheet__adds_entities_version(self):
-        self.assertPyxformXform(
-            name="data",
-            md="""
-            | survey   |         |       |       |
-            |          | type    | name  | label |
-            |          | text    | a     | A     |
-            | entities |         |       |       |
-            |          | dataset | label |       |
-            |          | trees   | a     |       |
-            """,
-            xml__xpath_match=[
-                '/h:html/h:head/x:model[@entities:entities-version = "2022.1.0"]'
-            ],
         )
 
     def test_entities_version__omitted_if_no_entities_sheet(self):
