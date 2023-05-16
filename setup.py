66d5e89c2e13ae80dd4affcfaca3ccd40ff2f0c0
Hélène Martin <ln@nafundi.com>
2023-05-16 09:03:07 -0700

    Prepare 1.12.1 release (#645)

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="pyxform",
-    version="1.12.0",
+    version="1.12.1",
     author="github.com/xlsform",
     author_email="info@xlsform.org",
     packages=find_packages(exclude=["tests", "tests.*"]),
