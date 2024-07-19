e2de7626c587aa52aa1b92058eb0e3b7092f5b28
lindsay stevens <lstevens@getodk.org>
2024-07-19 16:25:10 +1000

    fix: uncomment test cases that were left commented out accidentally

--- tests/xform_test_case/test_bugs.py
+++ tests/xform_test_case/test_bugs.py
@@ -24,12 +24,12 @@ class TestXFormConversion(TestCase):
         """Should find that conversion results in an error being raised by pyxform."""
         cases = (
             ("group_name_test.xls", "[row : 3] Question or group with no name."),
-            # (
-            #    "not_closed_group_test.xls",
-            #    "Unmatched begin statement: group (open_group_1)",
-            # ),
-            # ("duplicate_columns.xlsx", "Duplicate column header: label"),
-            # ("calculate_without_calculation.xls", "[row : 34] Missing calculation."),
+            (
+                "not_closed_group_test.xls",
+                "Unmatched begin statement: group (open_group_1)",
+            ),
+            ("duplicate_columns.xlsx", "Duplicate column header: label"),
+            ("calculate_without_calculation.xls", "[row : 34] Missing calculation."),
         )
         for i, (case, err_msg) in enumerate(cases):
             with self.subTest(msg=f"{i}: {case}"):
