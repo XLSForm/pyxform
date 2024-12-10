5b25fa281c508ec5e2baf18abc1e89eb6de29fd6
Hélène Martin <ln@getodk.org>
2024-12-09 19:48:02 -0800

    Prepare 3.0.0 release (#744)

--- pyxform/__init__.py
+++ pyxform/__init__.py
@@ -4,7 +4,7 @@ pyxform is a Python library designed to make authoring XForms for ODK
 Collect easy.
 """
 
-__version__ = "2.2.0"
+__version__ = "3.0.0"
 
 from pyxform.builder import (
     SurveyElementBuilder,
